4d2ba66676b070bd71c8f3f95c4dc3d1776e1fd1
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-06-05 09:25:50 +0700

    Update options.js

--- assets/js/options.js
+++ assets/js/options.js
@@ -16,7 +16,7 @@ const OPTIONS = {
         "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira",
         "Stikes Dharma Husada Bandung (DHB)","Inkes Rajawali","Inkes Immanuel","Inkes Pelamonia Makasar","Poltekkes Palu","Poltekkes Bandung","Politeknik Cendrawasih Palu","Universitas Padjajaran (UNPAD)",
         "Universitas Pasundan (UNPAS)","Universitas Sali Al-Aitam","UNISA Bandung","Universitas Bhakti Kencana (UBK)","Universitas Pendidikan Indonesia (UPI)","Universitas Jenderal Achmad Yani (UNJANI)",
-        "Universitas Borromeus","Universitas Muhammadiyah Ahmad Dahlan (UM-AD) Palembang","Universitas Dehasen (UNIVED) Bengkulu","Stikes Marendeng Majene"
+        "Universitas Borromeus","Universitas Muhammadiyah Ahmad Dahlan (UM-AD) Palembang","Universitas Dehasen (UNIVED) Bengkulu","Stikes Marendeng Majene","Poltekkes Surakarta"
     ],
 
     jurusan: [

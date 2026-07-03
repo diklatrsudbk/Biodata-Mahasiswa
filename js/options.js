55ae72f951c2796613b70c26b5c792d9330f104f
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-07-03 22:15:27 +0700

    Refactor OPTIONS object structure

--- js/options.js
+++ js/options.js
@@ -13,7 +13,7 @@ const OPTIONS = {
     universitas: [
         "ATRO Persada Nusantara Bekasi", "Binus University Bandung","Institut Teknologi Bandung (ITB)","Politeknik PIKSI Ganesha","Politkenik Al Islam Bandung","Universitas Indonesia (UI)","Sekolah Tinggi Farmasi Indonesia (STFI)",
         "Universitas Muhammadiyah Bandung (UMB)","Universitas Wahid Hasyim Semarang (UNWAHAS)","UIN Sunan Gunung Djati Bandung","Poltekkes Semarang","Poltekkes Tasikmalaya","SMKN 7 Bandung","SMK Nusa Bhakti Bandung",
-        "Universitas Diponegoro (UNDIP)","Universitas Islam Bandung (UNISBA)","Universitas Maranatha","Universitas Sriwijaya", "Universitas Siliwangi Tasikmalaya",
+        "Universitas Diponegoro (UNDIP)","Universitas Islam Bandung (UNISBA)","Universitas Maranatha","Universitas Sriwijaya", "Universitas Siliwangi Tasikmalaya", "SMK Angkasa",
         "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira",
         "Stikes Dharma Husada Bandung (DHB)","Inkes Rajawali","Inkes Immanuel","Inkes Pelamonia Makasar","Poltekkes Palu","Poltekkes Bandung","Politeknik Cendrawasih Palu","Universitas Padjajaran (UNPAD)",
         "Universitas Pasundan (UNPAS)","Universitas Sali Al-Aitam","UNISA Bandung","Universitas Bhakti Kencana (UBK)","Universitas Pendidikan Indonesia (UPI)","Universitas Jenderal Achmad Yani (UNJANI)",

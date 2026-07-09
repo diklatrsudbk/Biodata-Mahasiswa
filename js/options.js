ad6d6325f04a99d1c3e67dc4f304f9e4db9602d4
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-07-09 12:29:05 +0700

    Update options.js

--- js/options.js
+++ js/options.js
@@ -14,7 +14,7 @@ const OPTIONS = {
         "ATRO Persada Nusantara Bekasi", "Binus University Bandung","Institut Teknologi Bandung (ITB)","Politeknik PIKSI Ganesha","Politkenik Al Islam Bandung","Universitas Indonesia (UI)","Sekolah Tinggi Farmasi Indonesia (STFI)",
         "Universitas Muhammadiyah Bandung (UMB)","Universitas Wahid Hasyim Semarang (UNWAHAS)","UIN Sunan Gunung Djati Bandung","Poltekkes Semarang","Poltekkes Tasikmalaya","SMKN 7 Bandung","SMK Nusa Bhakti Bandung",
         "Universitas Diponegoro (UNDIP)","Universitas Islam Bandung (UNISBA)","Universitas Maranatha","Universitas Sriwijaya", "Universitas Siliwangi Tasikmalaya", "SMK Angkasa", "Poltekkes Jakarta II",
-        "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira",
+        "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira","SMK LPPM RI",
         "Stikes Dharma Husada Bandung (DHB)","Inkes Rajawali","Inkes Immanuel","Inkes Pelamonia Makasar","Poltekkes Palu","Poltekkes Bandung","Politeknik Cendrawasih Palu","Universitas Padjajaran (UNPAD)",
         "Universitas Pasundan (UNPAS)","Universitas Sali Al-Aitam","UNISA Bandung","Universitas Bhakti Kencana (UBK)","Universitas Pendidikan Indonesia (UPI)","Universitas Jenderal Achmad Yani (UNJANI)",
         "Universitas Borromeus","Universitas Muhammadiyah Ahmad Dahlan (UM-AD) Palembang","Universitas Dehasen (UNIVED) Bengkulu","Stikes Marendeng Majene","Poltekkes Surakarta", "Pelatihan NICU", "Akademi Kebidanan Konawe"
@@ -24,7 +24,7 @@ const OPTIONS = {
         "Keperawatan Regular","Keperawatan Non-Regular","Keperawatan RPL","Profesi Ners","Profesi Ners Non-Regular","Kebidanan Regular","Kebidanan Non-regular","Kebidanan RPL","Profesi Bidan Regular", "Teknik Elektromedik",
         "Profesi Bidan Non-Regular","Kedokteran","Kedokteran / PSPD","Kedokteran PIDI","Kedokteran PIDGI","PPDS Anak","PPDS Anaestesi","PPDS Bedah","PPDS Obgyn","PPDS KFR","PPDS THT-KL","PPDS Neurologi","PPDS IPD","PPDS DVE","Kedokteran Gigi","Anestesi Regular",
         "Anestesi RPL","Laboratorium Medis","Gizi","Psikologi","Fisioterapi","Analis Kesehatan","Rekam Medik","Terapi Wicara","Terapi Okupasi","Fisika","Biologi","Radiologi","Farmasi","Computer Science","Apoteker",
-        "Kesehatan Lingkungan (KESLING)", "Kesehatan Masyarakat", "Rekayasa Perangkat Lunak"
+        "Kesehatan Lingkungan (KESLING)", "Kesehatan Masyarakat", "Rekayasa Perangkat Lunak", "Teknik Jaringan Komputer dan Telekomunikasi"
     ]
 
 };

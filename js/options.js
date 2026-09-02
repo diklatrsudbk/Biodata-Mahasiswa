6f56f5133115658e208f456eb5573ccc816200bf
diklatrsudbk <diklatrsudbk2025@gmail.com>
2026-09-02 17:08:59 +0700

    Refactor OPTIONS object structure in options.js

--- js/options.js
+++ js/options.js
@@ -13,8 +13,8 @@ const OPTIONS = {
     universitas: [
         "ATRO Persada Nusantara Bekasi", "Binus University Bandung","Institut Teknologi Bandung (ITB)","Politeknik PIKSI Ganesha","Politkenik Al Islam Bandung","Universitas Indonesia (UI)","Sekolah Tinggi Farmasi Indonesia (STFI)",
         "Universitas Muhammadiyah Bandung (UMB)","Universitas Wahid Hasyim Semarang (UNWAHAS)","UIN Sunan Gunung Djati Bandung","Poltekkes Semarang","Poltekkes Tasikmalaya","SMKN 7 Bandung","SMK Nusa Bhakti Bandung",
-        "Universitas Diponegoro (UNDIP)","Universitas Islam Bandung (UNISBA)","Universitas Maranatha","Universitas Sriwijaya", "Universitas Siliwangi Tasikmalaya", "SMK Angkasa", "Poltekkes Jakarta II",
-        "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira","SMK LPPM RI",
+        "Universitas Diponegoro (UNDIP)","Universitas Islam Bandung (UNISBA)","Universitas Maranatha","Universitas Sriwijaya", "Universitas Siliwangi Tasikmalaya", "SMK Angkasa", "Poltekkes Jakarta II","Universitas Bale Bandung",
+        "Universitas Telkom","Stikes Karsa Husada Garut","Universitas Logistik dan Bisnis Internasional (ULBI)","Universitas Bandung","Stikep PPNI Jabar","Stikes Budi Luhur","Stikes RS Dustira","SMK LPPM RI","Universitas Widyatama",
         "Stikes Dharma Husada Bandung (DHB)","Inkes Rajawali","Inkes Immanuel","Inkes Pelamonia Makasar","Poltekkes Palu","Poltekkes Bandung","Politeknik Cendrawasih Palu","Universitas Padjajaran (UNPAD)","STMIK Mardira Indonesia",
 ,        "Universitas Pasundan (UNPAS)","Universitas Sali Al-Aitam","UNISA Bandung","Universitas Bhakti Kencana (UBK)","Universitas Pendidikan Indonesia (UPI)","Universitas Jenderal Achmad Yani (UNJANI)", "Universitas Komputer Indonesia (UNIKOM)",
         "Universitas Borromeus","Universitas Muhammadiyah Ahmad Dahlan (UM-AD) Palembang","Universitas Dehasen (UNIVED) Bengkulu","Stikes Marendeng Majene","Poltekkes Surakarta", "Pelatihan NICU", "Akademi Kebidanan Konawe"
